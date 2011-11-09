aadb40126b4902472ea2bb302b71e7b531a16c1d
Christopher J. Madsen <perl@cjmweb.net>
2011-11-09 17:30:09 -0600

    Just list GPL 2 in metadata (not 2 & 3)
    The license is still GPL 2 or any later version, but the CPAN Meta
    spec doesn't have a good way to list that.

--- Setupsup.pm
+++ Setupsup.pm
@@ -357,7 +357,7 @@ __END__
 
 =head1 SYNOPSIS
 
-        use Win32::Setupsup;
+  use Win32::Setupsup;
 
 
 =head1 DESCRIPTION
@@ -779,4 +779,3 @@ This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation; either version 2 of the License, or
 (at your option) any later version.
-
